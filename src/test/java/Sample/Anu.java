5547157d7c563edbf2ad03a5d6e992c00d2549a4
anusha <anushasnallur@gmail.com>
2023-01-24 12:16:19 +0530

    abc

---
+++ src/test/java/Sample/Anu.java
@@ -0,0 +1,18 @@
+package Sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Anu 
+{
+	@Test(groups = "smoke")
+	public void m3()
+	{
+		System.out.println("Hii gud mrng");
+		String b = System.getProperty("browser");
+		System.out.println(b);
+		String u  =System.getProperty("url");
+		System.out.println(u);
+	}
+}
---
+++ src/test/java/Sample/NavyaTest.java
@@ -0,0 +1,15 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class NavyaTest {
+
+	@Test
+	public void m1()
+	{
+		// TODO Auto-generated method stub
+		System.out.println("This is Navya Suvarna");
+
+	}
+
+}
